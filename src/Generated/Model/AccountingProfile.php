ac4fa7c2db327055739d7cf44a4117e1b6105b76
sandra.kocic <sandra.kocic@datenkraft.com>
2021-09-14 14:40:54 +0200

    BB-1252 regenerate client

---
+++ src/Generated/Model/AccountingProfile.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Datenkraft\Backbone\Client\AccountingProfileApi\Generated\Model;
+
+class AccountingProfile
+{
+    /**
+     * Accounting Profile Id
+     *
+     * @var string
+     */
+    protected $accountingProfileId;
+    /**
+     * Name
+     *
+     * @var string
+     */
+    protected $name;
+    /**
+     * Accounting Profile Id
+     *
+     * @return string
+     */
+    public function getAccountingProfileId() : string
+    {
+        return $this->accountingProfileId;
+    }
+    /**
+     * Accounting Profile Id
+     *
+     * @param string $accountingProfileId
+     *
+     * @return self
+     */
+    public function setAccountingProfileId(string $accountingProfileId) : self
+    {
+        $this->accountingProfileId = $accountingProfileId;
+        return $this;
+    }
+    /**
+     * Name
+     *
+     * @return string
+     */
+    public function getName() : string
+    {
+        return $this->name;
+    }
+    /**
+     * Name
+     *
+     * @param string $name
+     *
+     * @return self
+     */
+    public function setName(string $name) : self
+    {
+        $this->name = $name;
+        return $this;
+    }
+}
