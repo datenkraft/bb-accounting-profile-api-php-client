f9a7cea589f35674865e2cae815bf5ffc6fb8238
CircleCI <office@datenkraft.com>
2023-05-11 14:58:05 +0000

    Autogenerated client update [publish]

--- src/Generated/Normalizer/NewAuthRoleResourceNormalizer.php
+++ src/Generated/Normalizer/NewAuthRoleResourceNormalizer.php
@@ -11,18 +11,18 @@ use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-class PaymentTermsNormalizer implements DenormalizerInterface, NormalizerInterface, DenormalizerAwareInterface, NormalizerAwareInterface
+class NewAuthRoleResourceNormalizer implements DenormalizerInterface, NormalizerInterface, DenormalizerAwareInterface, NormalizerAwareInterface
 {
     use DenormalizerAwareTrait;
     use NormalizerAwareTrait;
     use CheckArray;
     public function supportsDenormalization($data, $type, $format = null) : bool
     {
-        return $type === 'Datenkraft\\Backbone\\Client\\AccountingProfileApi\\Generated\\Model\\PaymentTerms';
+        return $type === 'Datenkraft\\Backbone\\Client\\AccountingProfileApi\\Generated\\Model\\NewAuthRoleResource';
     }
     public function supportsNormalization($data, $format = null) : bool
     {
-        return is_object($data) && get_class($data) === 'Datenkraft\\Backbone\\Client\\AccountingProfileApi\\Generated\\Model\\PaymentTerms';
+        return is_object($data) && get_class($data) === 'Datenkraft\\Backbone\\Client\\AccountingProfileApi\\Generated\\Model\\NewAuthRoleResource';
     }
     /**
      * @return mixed
@@ -35,21 +35,12 @@ class PaymentTermsNormalizer implements DenormalizerInterface, NormalizerInterfa
         if (isset($data['$recursiveRef'])) {
             return new Reference($data['$recursiveRef'], $context['document-origin']);
         }
-        $object = new \Datenkraft\Backbone\Client\AccountingProfileApi\Generated\Model\PaymentTerms();
+        $object = new \Datenkraft\Backbone\Client\AccountingProfileApi\Generated\Model\NewAuthRoleResource();
         if (null === $data || false === \is_array($data)) {
             return $object;
         }
-        if (\array_key_exists('paymentTermsId', $data)) {
-            $object->setPaymentTermsId($data['paymentTermsId']);
-        }
         if (\array_key_exists('name', $data)) {
             $object->setName($data['name']);
-        }
-        if (\array_key_exists('billingInterval', $data)) {
-            $object->setBillingInterval($data['billingInterval']);
-        }
-        if (\array_key_exists('accountingProfileId', $data)) {
-            $object->setAccountingProfileId($data['accountingProfileId']);
         }
         return $object;
     }
@@ -59,18 +50,7 @@ class PaymentTermsNormalizer implements DenormalizerInterface, NormalizerInterfa
     public function normalize($object, $format = null, array $context = array())
     {
         $data = array();
-        if (null !== $object->getPaymentTermsId()) {
-            $data['paymentTermsId'] = $object->getPaymentTermsId();
-        }
-        if (null !== $object->getName()) {
-            $data['name'] = $object->getName();
-        }
-        if (null !== $object->getBillingInterval()) {
-            $data['billingInterval'] = $object->getBillingInterval();
-        }
-        if (null !== $object->getAccountingProfileId()) {
-            $data['accountingProfileId'] = $object->getAccountingProfileId();
-        }
+        $data['name'] = $object->getName();
         return $data;
     }
 }
