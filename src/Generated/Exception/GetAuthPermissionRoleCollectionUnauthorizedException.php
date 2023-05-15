39764e8b23ed159a43b2298f5750236e31293930
CircleCI <office@datenkraft.com>
2023-05-15 14:47:44 +0000

    Autogenerated client update [publish]

--- src/Generated/Exception/GetAuthPermissionRoleCollectionUnauthorizedException.php
+++ src/Generated/Exception/GetAuthPermissionRoleCollectionUnauthorizedException.php
@@ -2,7 +2,7 @@
 
 namespace Datenkraft\Backbone\Client\AccountingProfileApi\Generated\Exception;
 
-class PutPaymentTermsForbiddenException extends ForbiddenException
+class GetAuthPermissionRoleCollectionUnauthorizedException extends UnauthorizedException
 {
     /**
      * @var \Datenkraft\Backbone\Client\AccountingProfileApi\Generated\Model\ErrorResponse
@@ -10,7 +10,7 @@ class PutPaymentTermsForbiddenException extends ForbiddenException
     private $errorResponse;
     public function __construct(\Datenkraft\Backbone\Client\AccountingProfileApi\Generated\Model\ErrorResponse $errorResponse)
     {
-        parent::__construct('Forbidden');
+        parent::__construct('Unauthorized');
         $this->errorResponse = $errorResponse;
     }
     public function getErrorResponse() : \Datenkraft\Backbone\Client\AccountingProfileApi\Generated\Model\ErrorResponse
